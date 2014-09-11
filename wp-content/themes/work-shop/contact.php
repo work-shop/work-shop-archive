6d3cdc9aba9033d037ea9914d4b7c28239db0468
Greg Nemes <mail@gregnemes.com>
2014-09-11 18:05:37 -0400

    Contact Section Layout

--- wp-content/themes/work-shop/contact.php
+++ wp-content/themes/work-shop/contact.php
@@ -1,5 +1,5 @@
 
-<section id="contact" class="contact block min bg-light">
+<section id="contact" class="contact block min bg-light padded-more">
 
 	<div class="block-body">
 		<div class="container">
@@ -7,18 +7,26 @@
 			
 				<div class="col-sm-6 col-md-4">
 					<address>
-					
-					
-					
-					</address>
+						<h2>The Design Office</h2>
+						<h3>204 Westminster Street, Floor 3</h3>
+						<h3>Providence, RI 02903</h3>						
+					</address>				
 				</div>
 			
 				<div class="col-sm-6 col-md-4">
-				
+					<address>
+						<h2><a href="mailto:info@workshopri.com" target="_blank">info@workshopri.com</a></h2>
+						<h3>p. 440-915-2393</h3>
+						<h3>f. 401-274-1913</h3>						
+					</address>	
 				</div>
 				
 				<div class="col-sm-6 col-md-4">
-				
+					<address>
+						<h2>The Shop</h2>
+						<h3>610 Manton Avenue</h3>
+						<h3>Providence, RI 02909</h3>						
+					</address>					
 				</div>							
 	
 			</div>
