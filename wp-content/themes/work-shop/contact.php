bc2d1c24089f7e2efb4ad932e24c5e87d387a878
Greg Nemes <mail@gregnemes.com>
2014-09-11 15:40:50 -0400

    Home Page Content and Styling

--- wp-content/themes/work-shop/contact.php
+++ wp-content/themes/work-shop/contact.php
@@ -1,4 +1,45 @@
 
-<section id="contact" class="block">
+<section id="contact" class="contact block min bg-light">
+
+	<div class="block-body">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-sm-6 col-md-4">
+				
+				</div>
+			
+				<div class="col-sm-6 col-md-4">
+				
+				</div>
+				
+				<div class="col-sm-6 col-md-4">
+				
+				</div>							
+	
+			</div>
+		</div>
+	</div>
+	
+	<div class="block-body">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-sm-6">
+					<div class="button button-large">
+						<h3>Contact Us</h3>
+					</div>
+				
+				</div>
+			
+				<div class="col-sm-6">
+					<div class="button button-large">
+						<h3>Subscribe to our Newsletter</h3>
+					</div>				
+				</div>
+				
+			</div>
+		</div>
+	</div>	
 
 </section>
