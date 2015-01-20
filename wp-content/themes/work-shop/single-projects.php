01ee5521d36abaaf9c5f66dbe2004e2c268f2333
Greg Nemes <mail@gregnemes.com>
2015-01-20 11:44:31 -0500

    added project-edit button

--- wp-content/themes/work-shop/single-projects.php
+++ wp-content/themes/work-shop/single-projects.php
@@ -307,6 +307,8 @@
 	
 	<div id="project-dash" class="dash static"></div>		
 	
+	<?php if(is_user_logged_in()): ?><a href="<?php echo get_edit_post_link(); ?>" target="_blank" id="project-edit" class="hidden-xs"><span class="icon" data-icon="r"></span></a><?php endif; ?>
+	
 </div>	
 	
 <?php get_template_part('related'); ?>
